f4ffb76c8e18562d6d917211af55eae380a1258e
Marat Khalili <qm2k@yandex.ru>
2017-04-21 12:48:30 +0000

    General functionality of bash timer script.

--- timer_script.py
+++ timer_script.py
@@ -22,20 +22,40 @@ def parse_burp_duration(duration_string,
     return datetime.timedelta(seconds = int(match.group('number')) * __unit_to_seconds[match.group('unit')])
 
 
+def get_backup_timestamp(path):
+    if not os.path.exists(path):
+        return None
+    timestamp_filename = os.path.join(path, 'timestamp')
+    with open(timestamp_filename, 'rt') as timestamp_file:
+        line = timestamp_file.readline().strip('\n')
+        index, timestamp_string = line.split(' ', maxsplit = 1)
+        timestamp = datetime.datetime.strptime(timestamp_string, '%Y-%m-%d %H:%M:%S')
+        return timestamp
+
+
 def main(arguments):
     '''Main function.'''
-    if len(arguments) < 6 or '--help' in arguments:
-        sys.stderr.write('Usage: <client_name> <latest_path> <client_path> <reserverd1> <reserverd2>\n')
+    if len(arguments) < 7 or '--help' in arguments:
+        sys.stderr.write('Usage: <client_name> <latest_path> <data_path> <reserverd1> <reserverd2> <interval>\n')
         return os.EX_USAGE
+    client_name, latest_path, data_path, _, _, interval_string = arguments[1:7]
+    interval = parse_burp_duration(interval_string)
 
-    print('arguments:', arguments)
+    latest_timestamp = get_backup_timestamp(latest_path)
 
-    command = list(arguments)
-    command[0] = '/usr/share/burp/scripts/timer_script'
-    command[7:] = arguments[7:]
-    print('command:', command)
-    result = subprocess.run(command)
-    return result.returncode
+    print('Last backup: {}'.format(latest_timestamp))
+    if latest_timestamp:
+        print('Next after : {} (interval {})'.format(latest_timestamp + interval, interval_string))
+    else:
+        print('No prior backup of {}'.format(client_name))
+
+    current_timestamp = datetime.datetime.now()
+    if not latest_timestamp or latest_timestamp + interval < current_timestamp:
+        print('Do a backup of {} now.'.format(client_name))
+        return 0
+    else:
+        print('Not yet time for a backup of {}.'.format(client_name))
+        return 1
 
 
 if __name__ == "__main__":
