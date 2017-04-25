41ac43a920806462940287ce17ab55f0545e9f2e
Marat Khalili <qm2k@yandex.ru>
2017-04-25 15:05:14 +0000

    Refactored options handling.

---
+++ test_timer_script.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''Unit tests for the better burp timer script.'''
+
+
+import os
+import sys
+import timer_script
+import unittest
+
+import datetime
+
+
+
+TEST_BACKUPS = os.path.join(os.path.dirname(__file__), '_test_data', 'backups')
+
+def get_backup_path(backup_name = 'default'):
+    return os.path.join(TEST_BACKUPS, backup_name, 'current')
+
+
+class Test_parse_burp_duration(unittest.TestCase):
+
+    def test_constant(self):
+        assert timer_script.parse_burp_duration('3d') == datetime.timedelta(days = 3)
+
+
+class Test_is_backup_continued(unittest.TestCase):
+
+    def test_continued(self):
+        assert timer_script.is_backup_continued(get_backup_path('continued'))
+
+    def test_onepiece(self):
+        assert not timer_script.is_backup_continued(get_backup_path('onepiece'))
+
+
+class Test_get_backup_timestamp(unittest.TestCase):
+
+    def test_constant(self):
+        expected_timestamp = datetime.datetime.strptime('2017-04-05 12:32:07', '%Y-%m-%d %H:%M:%S')
+        assert timer_script.get_backup_timestamp(get_backup_path('timestamp')) == expected_timestamp
+
+
+class Test_is_backup_necessary(unittest.TestCase):
+
+    def setUp(self):
+        with open(os.path.join(get_backup_path('20h'), 'timestamp'), 'wt') as timestamp_file:
+            timestamp = datetime.datetime.now().replace(microsecond = 0) - datetime.timedelta(hours = 20)
+            timestamp_file.write('0000010 {}\n'.format(timestamp.isoformat(' ')))
+
+    def test_no_conditions(self):
+        assert not timer_script.is_backup_necessary(get_backup_path())
+
+    def test_continued(self):
+        assert timer_script.is_backup_necessary(get_backup_path('continued'))
+
+    def test_new_backup(self):
+        assert timer_script.is_backup_necessary(get_backup_path('empty'))
+
+    def test_lan(self):
+        backup_path = get_backup_path()
+
+        os.environ['REMOTE_ADDR'] = '10.10.10.10'
+        assert timer_script.is_backup_necessary(backup_path, '--lan')
+        assert not timer_script.is_backup_necessary(backup_path, '--not-lan')
+
+        os.environ['REMOTE_ADDR'] = '8.8.8.8'
+        assert not timer_script.is_backup_necessary(backup_path, '--lan')
+        assert timer_script.is_backup_necessary(backup_path, '--not-lan')
+
+    def test_weekday(self):
+        backup_path = get_backup_path()
+        import timer_script
+        saved_datetime = timer_script.CURRENT_DATETIME
+        try:
+            timer_script.CURRENT_DATETIME = datetime.datetime.strptime('2017-04-25 14:46:05', '%Y-%m-%d %H:%M:%S')
+            assert timer_script.is_backup_necessary(backup_path, '--workday')
+            assert not timer_script.is_backup_necessary(backup_path, '--holiday')
+
+            timer_script.CURRENT_DATETIME = datetime.datetime.strptime('2017-04-22 14:46:05', '%Y-%m-%d %H:%M:%S')
+            assert not timer_script.is_backup_necessary(backup_path, '--workday')
+            assert timer_script.is_backup_necessary(backup_path, '--holiday')
+
+            timer_script.CURRENT_DATETIME = datetime.datetime.strptime('2017-04-23 14:46:05', '%Y-%m-%d %H:%M:%S')
+            assert not timer_script.is_backup_necessary(backup_path, '--workday')
+            assert timer_script.is_backup_necessary(backup_path, '--holiday')
+        finally:
+            timer_script.CURRENT_DATETIME = saved_datetime
+
+    def test_age_exceeds(self):
+        backup_path = get_backup_path('20h')
+        assert timer_script.is_backup_necessary(backup_path, '--age-exceeds 19h')
+        assert not timer_script.is_backup_necessary(backup_path, '--age-exceeds 21h')
+
+    def test_binary_operations(self):
+        backup_path = get_backup_path('20h')
+        os.environ['REMOTE_ADDR'] = '10.10.10.10'
+
+        assert timer_script.is_backup_necessary(backup_path, '--age-exceeds 19h --lan')
+        assert not timer_script.is_backup_necessary(backup_path, '--age-exceeds 19h --not-lan')
+        assert not timer_script.is_backup_necessary(backup_path, '--age-exceeds 21h --lan')
+        assert not timer_script.is_backup_necessary(backup_path, '--age-exceeds 21h --not-lan')
+
+        assert timer_script.is_backup_necessary(backup_path, '--age-exceeds 19h', '--lan')
+        assert timer_script.is_backup_necessary(backup_path, '--age-exceeds 19h', '--not-lan')
+        assert timer_script.is_backup_necessary(backup_path, '--age-exceeds 21h', '--lan')
+        assert not timer_script.is_backup_necessary(backup_path, '--age-exceeds 21h', '--not-lan')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- timer_script.py
+++ timer_script.py
@@ -9,6 +9,7 @@ import sys
 
 import argparse
 import datetime
+import collections
 import gzip
 import ipaddress
 import re
@@ -29,8 +30,20 @@ def parse_burp_duration(duration_string,
     return datetime.timedelta(seconds = int(match.group('number')) * __unit_to_seconds[match.group('unit')])
 
 
-def get_backup_timestamp(path):
-    timestamp_filename = os.path.join(path, 'timestamp')
+def is_backup_continued(backup_path,
+    __interrupted_backup_regex = re.compile('\d{4}-\d\d-\d\d \d\d:\d\d:\d\d: burp\[\d+\] Found interrupted backup.\n'),
+):
+    log_filename = os.path.join(backup_path, 'log.gz')
+    with gzip.open(log_filename, 'rt') as log_file:
+        for line in log_file:
+            if __interrupted_backup_regex.fullmatch(line):
+                return True
+
+    return False
+
+
+def get_backup_timestamp(backup_path):
+    timestamp_filename = os.path.join(backup_path, 'timestamp')
     with open(timestamp_filename, 'rt') as timestamp_file:
         line = timestamp_file.readline().strip('\n')
         index, timestamp_string = line.split(' ', maxsplit = 1)
@@ -38,89 +51,77 @@ def get_backup_timestamp(path):
         return timestamp
 
 
-def get_argument_parser():
-    argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('interval', type = parse_burp_duration)
-    argument_parser.add_argument('--private-ip', action = 'store_true')
-    argument_parser.add_argument('--workday', action = 'store_true')
-    argument_parser.add_argument('--holiday', action = 'store_true')
-    argument_parser.add_argument('--hours-range', action = 'append')
-    return argument_parser
+Condition = collections.namedtuple('Condition', ('name', 'argument_action', 'call'))
 
 
-def match_hours_ranges(hours_ranges):
-    for hours_range in hours_ranges:
-        first_hour, last_hour = hours_range.split('..')
-        first_hour = int(first_hour) if first_hour else 0
-        last_hour = int(last_hour) if last_hour else 24
-        if first_hour <= CURRENT_DATETIME.hour <= last_hour:
-            return True
-    return False
+def match_argument_strings(latest_path, *argument_strings):
 
-
-def get_interval(argument_strings, __argument_parser = get_argument_parser()):
-    for argument_string in argument_strings:
-        arguments = __argument_parser.parse_args(shlex.split(argument_string))
-        if arguments.private_ip and not ipaddress.ip_address(os.environ['REMOTE_ADDR']).is_private:
-            continue
-        if arguments.workday and not CURRENT_DATETIME.weekday() < 5:
-            continue
-        if arguments.holiday and not CURRENT_DATETIME.weekday() >= 5:
-            continue
-        if arguments.hours_range and not match_hours_ranges(arguments.hours_range):
-            continue
-        return arguments.interval
-    raise ValueError('Interval cannot be determined', argument_strings)
-
-
-def is_backup_dubious(path,
-    __interrupted_backup_regex = re.compile('\d{4}-\d\d-\d\d \d\d:\d\d:\d\d: burp\[\d+\] Found interrupted backup.\n'),
-):
-    log_filename = os.path.join(path, 'log.gz')
-    with gzip.open(log_filename, 'rt') as log_file:
-        for line in log_file:
-            if __interrupted_backup_regex.fullmatch(line):
-                print('Backup was being interrupted.')
+    def match_hours_ranges(hours_ranges):
+        for hours_range in hours_ranges:
+            first_hour, last_hour = hours_range.split('..')
+            first_hour = int(first_hour) if first_hour else 0
+            last_hour = int(last_hour) if last_hour else 24
+            if first_hour <= CURRENT_DATETIME.hour <= last_hour:
                 return True
+        return False
+
+    def remote_address():
+        return ipaddress.ip_address(os.environ['REMOTE_ADDR'])
+
+    def weekday():
+        return CURRENT_DATETIME.weekday()
+
+    def age_exceeds(maximum_age_string):
+        return CURRENT_DATETIME > get_backup_timestamp(latest_path) + parse_burp_duration(maximum_age_string)
+
+    conditions = (
+        Condition(name = 'lan', argument_action = 'store_true', call = lambda: remote_address().is_private),
+        Condition(name = 'not_lan', argument_action = 'store_true', call = lambda: not remote_address().is_private),
+        Condition(name = 'workday', argument_action = 'store_true', call = lambda: weekday() < 5),
+        Condition(name = 'holiday', argument_action = 'store_true', call = lambda: weekday() >= 5),
+        Condition(name = 'age_exceeds', argument_action = 'store', call = age_exceeds),
+    )
+
+    parser = argparse.ArgumentParser()
+    for condition in conditions:
+        parser.add_argument('--' + condition.name.replace('_', '-'), action = condition.argument_action)
+
+    def match_conditions(arguments):
+        for condition in conditions:
+            argument_value = arguments.get(condition.name, None)
+            if argument_value in (None, False):
+                continue
+            condition_arguments = (argument_value,) if argument_value != True else ()
+            if not condition.call(*condition_arguments):
+                return False
+        return True
+
+    for argument_string in argument_strings:
+        if match_conditions(vars(parser.parse_args(shlex.split(argument_string)))):
+            return True
 
     return False
 
 
-def is_backup_necessary(latest_path, interval):
+def is_backup_necessary(latest_path, *argument_strings):
     if not os.path.exists(latest_path):
-        print('No prior backup.')
         return True
 
-    if is_backup_dubious(latest_path):
-        print('Prior backup is dubious.')
+    if is_backup_continued(latest_path):
         return True
 
-    latest_timestamp = get_backup_timestamp(latest_path)
-    print('Last backup: {}, interval: {}'.format(latest_timestamp, interval))
-
-    next_timestamp = latest_timestamp + interval
-    if next_timestamp < CURRENT_DATETIME:
-        return True
-
-    print('Next after : {}'.format(next_timestamp))
-    return False
+    return match_argument_strings(latest_path, *argument_strings)
 
 
 def main(arguments):
     '''Main function.'''
     if len(arguments) < 7 or '--help' in arguments:
-        sys.stderr.write('Usage: <client_name> <latest_path> <data_path> <reserverd1> <reserverd2> <argument_strings>\n')
+        sys.stderr.write('Usage: <client_name> <latest_path> <data_path> <reserverd1> <reserverd2> <argument_strings...>\n')
         return os.EX_USAGE
     client_name, latest_path, data_path = arguments[1:4]
     argument_strings = arguments[6:]
-    interval = get_interval(argument_strings)
 
-    backup_is_necessary = is_backup_necessary(latest_path, interval)
-    if backup_is_necessary:
-        print('Do backup now.'.format(client_name))
-        return os.EX_OK
-
-    return not os.EX_OK
+    return os.EX_OK if is_backup_necessary(latest_path, *argument_strings) else not os_EX_OK
 
 
 if __name__ == "__main__":
